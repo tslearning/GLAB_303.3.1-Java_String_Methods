6db2c9c44d531bc14d8f2fe9bdece7c70522fc59
Touyer Lee <touyer.lees@gmail.com>
2024-11-19 19:13:16 -0600

    Initial commit

---
+++ src/lengthDemo.java
@@ -0,0 +1,19 @@
+public class StringClass {
+    public static void main(String[] args) {
+
+    //1.	 length() method
+    //The length() method returns the length of the string,
+    // or it returns the count of the total number of characters present in the string
+        
+        String str1 = "Java";
+        String str2 = "";
+
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        System.out.println("Java".length());
+        System.out.println("Java\n".length());
+        System.out.println("Learn Java".length());
+    }
+
+
+}
